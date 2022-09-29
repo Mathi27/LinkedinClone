63f742bc1c7c9ccb143f183cf551ade154783ef3
Mathi27 <mathiyuvarajan.dev@gmail.com>
2022-09-29 15:14:15 +0530

    Firebase Cloud Messaging

--- lib/main.dart
+++ lib/main.dart
@@ -15,38 +15,18 @@ import 'package:linkedinui/MainScreen/jobs.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-AndroidNotificationChannel channel = AndroidNotificationChannel(
-  "LinkedIn",
-  "LinkedIn title",
-  importance: Importance.high,
-  playSound: true,
-);
-
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
-
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+Future<void> _firebaseMessagingBackgroundHandler(message) async {
   await Firebase.initializeApp();
-  print("A big Message just showed up: ${message.messageId}");
+  print('Handling a background message ${message.messageId}');
 }
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
-  await flutterLocalNotificationsPlugin
-      .resolvePlatformSpecificImplementation<
-          AndroidFlutterLocalNotificationsPlugin>()
-      ?.createNotificationChannel(channel);
-
-  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-    alert: true,
-    badge: true,
-    sound: true,
-  );
 
   runApp(LinkedIn());
 }
